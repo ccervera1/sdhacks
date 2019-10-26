636c52a04f2a8d83556540ff853d455da861e59e
audity <audreyshi123@gmail.com>
2019-10-26 11:42:55 -0700

    hi

--- wageCount.py
+++ wageCount.py
@@ -1,9 +1,11 @@
 import datetime
 import threading
 
+def everyFive():
+    threading.Timer(5.0, everyFive).start()
+    print("hi")
+
 current = datetime.datetime.now().time()
 print('You clocked in at ' + str(current))
 
-
-
-
+everyFive()
