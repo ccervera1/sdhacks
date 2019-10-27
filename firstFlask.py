9f254eef4c4b205b2d50d924d9993f0a9bfc8254
= <lenardk@uci.edu>
2019-10-26 17:24:34 -0700

    css

--- firstFlask.py
+++ firstFlask.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
 
-@app.route("/")
+
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/home")
 def home():
     return render_template("homeFlask.html")
 
-@app.route("/", methods=['POST'])
+
+
+@app.route("/home", methods=['POST'])
 def home_post():
-    #text = request.form['text']
+    # text = request.form['text']
     processed_text = 'hello'
-    #test_input(text)
+    # test_input(text)
     return processed_text
+
 
 if __name__ == "__main__":
     app.run(debug=True)
